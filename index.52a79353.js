395a02309067db0fa2e910969615500b419f5643
UserAndrii <113310900+UserAndrii@users.noreply.github.com>
2023-04-08 20:10:20 +0000

    Deploying to gh-pages from @ UserAndrii/trident@f2184c45cb0c076b8152487cc34e9c910deb9df3 🚀

---
+++ index.52a79353.js
@@ -0,0 +1,2 @@
+const e={menuBtn:document.querySelector(".menu-btn"),mobileMenu:document.querySelector("#mobile-menu"),body:document.querySelector("body")};e.menuBtn.addEventListener("click",(function(){e.menuBtn.classList.toggle("close"),e.mobileMenu.classList.toggle("is-open"),e.body.classList.toggle("modal-open")}));
+//# sourceMappingURL=index.52a79353.js.map
---
+++ index.69c0d4c3.js
@@ -0,0 +1,2 @@
+!function(){var e={menuBtn:document.querySelector(".menu-btn"),mobileMenu:document.querySelector("#mobile-menu"),body:document.querySelector("body")};e.menuBtn.addEventListener("click",(function(){e.menuBtn.classList.toggle("close"),e.mobileMenu.classList.toggle("is-open"),e.body.classList.toggle("modal-open")}))}();
+//# sourceMappingURL=index.69c0d4c3.js.map
